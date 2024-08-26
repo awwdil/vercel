4adbf91d9220f99c85556676257c5bf2b1b8caec
Awwdil <adil.khalil76@gmail.com>
2024-08-26 17:14:56 +0500

    added changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 // index.js
 module.exports = (req, res) => {
-    res.writeHead(301, { Location: 'https://www.highrevenuenetwork.com/fx0kp7362?key=02ba39ac6576dc4eff3fee2ec9764051' });
+    res.writeHead(301, { Location: 'https://www.google.com' });
     res.end();
   };
   
