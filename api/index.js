8ffae9eebcebd05e681930e4ad4564e03ae6e6c7
Awwdil <adil.khalil76@gmail.com>
2024-08-26 17:21:04 +0500

    added changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 // index.js
 module.exports = (req, res) => {
-    res.writeHead(301, { Location: 'https://www.google.com' });
+    res.writeHead(301, { Location: 'https://acceleratetomb.com/ch2yu1ntxr?key=1c9429d6408587765601d92fef00b289' });
     res.end();
   };
   
